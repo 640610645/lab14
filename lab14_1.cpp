cd4b2dd8b57f9d08118bf1560a5a430bb31f7a74
nchanadee <nontapan_chanadee@cmu.ac.th>
2021-12-28 17:48:39 +0700

    lab14.1

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,43 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    
+    int i;
+    double sum;
+    for(i = 0; i < n; i++){
+        sum += a[i];
+    }
+    b[0] = sum/n;
+
+    double SD;
+    for(i = 0; i < n; i++){
+        SD += pow(a[i] - (sum/n), 2);
+    }
+    b[1] = sqrt(SD/n);
+
+    double product = 1;
+    for(i = 0; i < n; i++){
+        product = product * a[i];
+    }
+    b[2] = pow(product,1.0/n);
+
+    double sumoffrac;
+    for(i = 0; i < n; i++){
+        sumoffrac += 1/a[i];
+    }
+    b[3] = n/sumoffrac;
+
+    double max = a[0],min = a[0];
+    for(i = 1; i < n; i++){
+        if(a[i] > max)
+        max = a[i];
+        if(a[i] < min)
+        min = a[i];
+    }
+    b[4] = max;
+    b[5] = min;
+}
+
+
